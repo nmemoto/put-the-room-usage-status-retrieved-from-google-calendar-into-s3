101567ff2d3d9239f934ab6481c02f0910b31a40
Takafumi Umemoto <takafumi.umemoto@gmail.com>
2021-01-05 15:40:43 +0000

    毎日午前6時にcdk.context.jsonで指定したcalendar idのユーザーの予定を実行日1日分取得してログに流す処理を作成

--- app/lambda/index.ts
+++ app/lambda/index.ts
@@ -1,5 +1,35 @@
-import { Context, ScheduledEvent } from 'aws-lambda';
+import { ScheduledEvent } from 'aws-lambda';
+import { addDays, format, parseISO } from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
+import { google } from 'googleapis';
+const credentials = require('../../credentials.json');
 
-export async function handler(event: ScheduledEvent, context: Context) {
-    console.info("handler")
+export async function handler(event: ScheduledEvent) {
+    try {
+        const utcDate = parseISO(event.time)
+        const jstDate = utcToZonedTime(utcDate, 'Asia/Tokyo')
+        const nextJstDate = addDays(jstDate, 1)
+        const date = format(jstDate,'yyyy-MM-dd')
+        const nextDate = format(nextJstDate,'yyyy-MM-dd')
+        console.info(utcDate, jstDate, nextJstDate, date, nextDate)
+        const calendarIds = process.env.CALENDAR_IDS!.split(',')
+        const auth = new google.auth.JWT(
+            credentials.client_email,
+            undefined,
+            credentials.private_key,
+            ['https://www.googleapis.com/auth/calendar']);
+        const calendar = google.calendar({version: 'v3', auth})
+
+        for(let id of calendarIds){
+            const list = await calendar.events.list({
+                calendarId: id,
+                timeMin: `${date}T00:00:00+09:00`,
+                timeMax: `${nextDate}T00:00:00+09:00`,
+                timeZone: 'Asia/Tokyo',
+            })
+            console.info(list.data.items)
+        }
+    } catch (err) {
+        console.error(err)
+    }
 }
--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -9,15 +9,20 @@ export class AppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const calendarIds: string[] = this.node.tryGetContext('calendarIds');
+
     const fn = new nodejs.NodejsFunction(this, 'TestFunction', {
       handler: 'handler',
       entry: 'app/lambda/index.ts',
       runtime: lambda.Runtime.NODEJS_12_X,
-      logRetention: logs.RetentionDays.ONE_MONTH
+      logRetention: logs.RetentionDays.ONE_MONTH,
+      environment: {
+        "CALENDAR_IDS": calendarIds.join(",")
+      }
    });
 
    const rule = new Rule(this, 'TestFunctionRule', {
-      schedule: Schedule.expression("cron(0/1 * * * ? *)"),
+      schedule: Schedule.expression("cron(0 21 * * ? *)"),
       targets: [
         new eventsTargets.LambdaFunction(fn)
       ]
